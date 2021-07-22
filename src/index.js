6f4168ea296168f8e4cf5739b8f6ba8f1c0bf192
Gaurav Gulati <56128284+gauravgulati15@users.noreply.github.com>
2021-07-22 11:15:50 +0530

    Updated environment variables

--- src/index.js
+++ src/index.js
@@ -6,13 +6,11 @@ import * as serviceWorker from "./serviceWorker";
 import { GithubProvider } from "./context/context";
 import { Auth0Provider } from "@auth0/auth0-react";
 
-const { REACT_APP_DOMAIN, REACT_APP_CLIENT_ID } = process.env;
-
 ReactDOM.render(
   <React.StrictMode>
     <Auth0Provider
-      domain={REACT_APP_DOMAIN}
-      clientId={REACT_APP_CLIENT_ID}
+      domain={process.env.REACT_APP_DOMAIN}
+      clientId={process.env.REACT_APP_CLIENT_ID}
       redirectUri={window.location.origin}
       cacheLocation="localstorage"
     >
